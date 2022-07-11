78bb7520c5ea842c7e751fb2d02d15157baa2b4f
Nicolás Cammarota <76768475+Nikong22@users.noreply.github.com>
2022-07-11 00:41:37 -0300

    first commit

--- options/mongoDB.js
+++ options/mongoDB.js
@@ -4,23 +4,11 @@ const URI = 'mongodb://localhost:27017/coder';
 
 const optionsDB = mongoose.connect(URI,
     {
-      serverSelectionTimeoutMS: 1000
+      serverSelectionTimeoutMS: 5000
     },
     (error) => {
       if (error) {
         throw 'Error al conectarse a la base de datos';
-      } else {
-        ProductoDB.find({})
-          .then((productosDB) => {
-            for (let producto of productosDB) {
-            }
-          })
-        MensajeDB.find({})
-          .then((mensajesDB) => {
-            for (let mensaje of mensajesDB) {
-            }
-            console.log("Conectado a la base de datos mongo...")
-          })
       }
     });
 
